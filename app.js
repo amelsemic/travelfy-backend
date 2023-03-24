b339cbba12169065e6cbe84e14da50250f8ece24
amelsemic <semicamel@hotmail.com>
2023-03-24 10:09:56 +0100

    app listen changed

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cnozerd.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   ) //mern ubaceno
   .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT ||  5000);
     console.log("connected to database!!");
   })
   .catch((err) => {
